207e6747d06d41e2de6760defc8cda0770faec6e
Tanmai Gopal <tanmaig@34cross.in>
2017-06-27 01:33:17 +0530

    Adds anonymous read hook.

--- app/src/server.js
+++ app/src/server.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
 
-//your routes here
-app.get('/', function (req, res) {
-    res.send("Hello World!");
+app.use(morgan('dev'));
+
+/* This webhook expects 2 query parameters:
+ * file_id, file_op
+ *
+ * This webhook allows any logged-in user (via the role: user) to upload,
+ * and any user (including anonymous) to download
+ *
+ */
+app.get('/public-read', function (req, res) {
+  const fileId = req.query.file_id;
+  const fileOp = req.query.file_op;
+  const roles = req.get('x-hasura-allowed-roles');
+  const isUser = (roles.indexOf('user') >= 0);
+  const isWriteOp = (['create', 'delete'].indexOf(fileOp) >= 0);
+  if (isWriteOp) {
+    if (isUser) {
+      res.send(JSON.stringify({message: 'allow'}));
+    } else {
+      res.status(403).send(JSON.stringify({message: 'deny'}));
+    }
+  } else {
+    res.send(JSON.stringify({message: 'allow'}));
+  }
 });
 
 app.listen(8080, function () {
